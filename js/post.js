a3f2b32bede80be2addaa8a26abd4202f05ac7ee
jonaslod <95305401+jonaslod@users.noreply.github.com>
2022-09-28 12:39:14 +0200

    Update about.html, contact.html, index.html, post.html, posts.html, post.js and displayPostShowcase.js

--- js/post.js
+++ js/post.js
@@ -28,8 +28,8 @@ try {
 
     pageContent.innerHTML = `
         <h1>${title}</h1>
-        <div class="categories"><img src="../images/icon/category-icon.png" alt="List of categories"/>${findInCategories(postCategories, categories)}</div>
-        <div class="date"><img src="images/icon/date-icon.png" alt="Post date"/>${formatDate(post.date)}</div>
+        <div class="categories"><img src="../images/icon/category-icon.png" alt="List of categories">${findInCategories(postCategories, categories)}</div>
+        <div class="date"><img src="images/icon/date-icon.png" alt="Post date">${formatDate(post.date)}</div>
         ${content}
     `;
 
@@ -39,20 +39,20 @@ try {
         });
     });
 
-    function showModal(image){
+    function showModal(image) {
         modal.style.display = "grid";
         modal.innerHTML = `
             <div class="close-modal" title="Close image">
-                <img src="images/icon/exit-icon.png" alt="Close modal" />
+                <img src="images/icon/exit-icon.png" alt="Close modal">
             </div>
             <div class="img-wrapper">
-                <img class="image-showcase" src="${image.src}" alt="${image.alt}" />
+                <img class="image-showcase" src="${image.src}" alt="${image.alt}">
             </div>
         `;
     }
 
     modal.addEventListener("click", (event) => {
-        if(event.target.className != "image-showcase"){
+        if (event.target.className != "image-showcase") {
             modal.style.display = "none";
         }
     });
@@ -65,13 +65,13 @@ catch {
     showError(pageContent, "Blog post could not be found.");
 }
 
-async function postComment(event){
+async function postComment(event) {
     event.preventDefault();
 
     const nameInput = document.querySelector("#author_name").value;
     const commentInput = document.querySelector("#content").value;
 
-    if(nameInput.trim() && commentInput.trim()){
+    if (nameInput.trim() && commentInput.trim()) {
         feedback.innerHTML = `<span class="italic">Loading ...</span>`;
         try {
             const url = `${baseUrl}comments?post=${id}`;
@@ -89,18 +89,18 @@ async function postComment(event){
                     "Authorization": `Basic ${btoa(username + ":" + applicationPassword)}`
                 }
             }
-    
+
             const responseStatus = await postToApi(url, options);
-            
-            if(responseStatus === 201){
+
+            if (responseStatus === 201) {
                 feedback.innerHTML = `<span class="success">Comment posted!</span>`;
                 form.reset();
                 showComments();
             }
-            else if(responseStatus === 409){
+            else if (responseStatus === 409) {
                 feedback.innerHTML = `<span class="error">Could not post comment, since comment already exists!</span>`;
             }
-            else{
+            else {
                 feedback.innerHTML = `<span class="error">Something went wrong when trying to post your comment, try again later.</span>`;
             }
 
@@ -109,17 +109,17 @@ async function postComment(event){
             showError(feedback, "An error occurred while trying to post your comment, try again later.");
         }
     }
-    else{
+    else {
         feedback.innerHTML = `<span class="error">Please enter your name and a comment</span>`;
     }
 }
 
-async function showComments(){
+async function showComments() {
     commentsWrapper.innerHTML = "";
     const postedComments = await fetchFromApi(`${baseUrl}comments?post=${id}`);
 
     try {
-        if(typeof postedComments === "object" && postedComments.length > 0){
+        if (typeof postedComments === "object" && postedComments.length > 0) {
             postedComments.forEach((postedComment) => {
                 const authorName = document.createElement("span");
                 authorName.setAttribute("class", "bold");
@@ -138,7 +138,7 @@ async function showComments(){
 
                 const authorWrapper = document.createElement("div");
                 authorWrapper.setAttribute("class", "author");
-                authorWrapper.innerHTML = `<div class="profile-image"><img src="images/icon/profile-icon.png" alt="Profile icon"/></div>`;
+                authorWrapper.innerHTML = `<div class="profile-image"><img src="images/icon/profile-icon.png" alt="Profile icon"></div>`;
                 authorWrapper.appendChild(authorDetails);
 
                 const commentWrapper = document.createElement("p");
@@ -149,12 +149,12 @@ async function showComments(){
                 currentComment.setAttribute("class", "comment");
                 currentComment.appendChild(authorWrapper);
                 currentComment.appendChild(commentWrapper);
-                
+
                 commentsWrapper.appendChild(currentComment);
             });
         }
-    
-        else{
+
+        else {
             commentsWrapper.innerHTML = `
             <div class="comment">
                 <p>There are currently no comments in this post.</p>
